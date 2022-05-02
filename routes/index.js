2f23d86b9f07b7cf2260b1081d2476dcbecdcd85
Ahmed Onour <ahmedonour.49@gmail.com>
2022-05-02 05:38:18 +0200

    fixing the database bug

--- routes/index.js
+++ routes/index.js
@@ -50,15 +50,16 @@ router.get('/completed', ensureLoggedIn, fetchTodos, function(req, res, next) {
 });
 
 router.post('/', ensureLoggedIn, function(req, res, next) {
-  req.body.title = req.body.title.trim();
+  req.body.title = req.body.title;
   next();
 }, function(req, res, next) {
   if (req.body.title !== '') { return next(); }
   return res.redirect('/' + (req.body.filter || ''));
 }, function(req, res, next) {
-  db.run('INSERT INTO houses (owner_id, title, completed) VALUES (?, ?, ?)', [
+  db.run('INSERT INTO houses (owner_id, discription, photo, completed) VALUES (?, ?, ?, ?)', [
     req.user.id,
-    req.body.title,
+    req.body.discription,
+    req.body.file,
     req.body.completed == true ? 1 : null
   ], function(err) {
     if (err) { return next(err); }
@@ -66,58 +67,58 @@ router.post('/', ensureLoggedIn, function(req, res, next) {
   });
 });
 
-//router.post('/:id(\\d+)', ensureLoggedIn, function(req, res, next) {
-//  req.body.title = req.body.title.trim();
-//  next();
-//}, function(req, res, next) {
-//  if (req.body.title !== '') { return next(); }
-//  db.run('DELETE FROM todos WHERE rowid = ? AND owner_id = ?', [
-//    req.params.id,
-//    req.user.id
-//  ], function(err) {
-//    if (err) { return next(err); }
-//    return res.redirect('/' + (req.body.filter || ''));
-//  });
-//}, function(req, res, next) {
-//  db.run('UPDATE todos SET title = ?, completed = ? WHERE rowid = ? AND owner_id = ?', [
-//    req.body.title,
-//    req.body.completed !== undefined ? 1 : null,
-//    req.params.id,
-//    req.user.id
-//  ], function(err) {
-//    if (err) { return next(err); }
-//    return res.redirect('/' + (req.body.filter || ''));
-//  });
-//});
-//
-//router.post('/:id(\\d+)/delete', ensureLoggedIn, function(req, res, next) {
-//  db.run('DELETE FROM todos WHERE rowid = ? AND owner_id = ?', [
-//    req.params.id,
-//    req.user.id
-//  ], function(err) {
-//    if (err) { return next(err); }
-//    return res.redirect('/' + (req.body.filter || ''));
-//  });
-//});
-//
-//router.post('/toggle-all', ensureLoggedIn, function(req, res, next) {
-//  db.run('UPDATE todos SET completed = ? WHERE owner_id = ?', [
-//    req.body.completed !== undefined ? 1 : null,
-//    req.user.id
-//  ], function(err) {
-//    if (err) { return next(err); }
-//    return res.redirect('/' + (req.body.filter || ''));
-//  });
-//});
-//
-//router.post('/clear-completed', ensureLoggedIn, function(req, res, next) {
-//  db.run('DELETE FROM todos WHERE owner_id = ? AND completed = ?', [
-//    req.user.id,
-//    1
-//  ], function(err) {
-//    if (err) { return next(err); }
-//    return res.redirect('/' + (req.body.filter || ''));
-//  });
-//});
+router.post('/:id(\\d+)', ensureLoggedIn, function(req, res, next) {
+ req.body.title = req.body.title.trim();
+ next();
+}, function(req, res, next) {
+ if (req.body.title !== '') { return next(); }
+ db.run('DELETE FROM houses WHERE rowid = ? AND owner_id = ?', [
+   req.params.id,
+   req.user.id
+ ], function(err) {
+   if (err) { return next(err); }
+   return res.redirect('/' + (req.body.filter || ''));
+ });
+}, function(req, res, next) {
+ db.run('UPDATE houses SET title = ?, completed = ? WHERE rowid = ? AND owner_id = ?', [
+   req.body.title,
+   req.body.completed !== undefined ? 1 : null,
+   req.params.id,
+   req.user.id
+ ], function(err) {
+   if (err) { return next(err); }
+   return res.redirect('/' + (req.body.filter || ''));
+ });
+});
+
+router.post('/:id(\\d+)/delete', ensureLoggedIn, function(req, res, next) {
+ db.run('DELETE FROM houses WHERE rowid = ? AND owner_id = ?', [
+   req.params.id,
+   req.user.id
+ ], function(err) {
+   if (err) { return next(err); }
+   return res.redirect('/' + (req.body.filter || ''));
+ });
+});
+
+router.post('/toggle-all', ensureLoggedIn, function(req, res, next) {
+ db.run('UPDATE houses SET completed = ? WHERE owner_id = ?', [
+   req.body.completed !== undefined ? 1 : null,
+   req.user.id
+ ], function(err) {
+   if (err) { return next(err); }
+   return res.redirect('/' + (req.body.filter || ''));
+ });
+});
+
+router.post('/clear-completed', ensureLoggedIn, function(req, res, next) {
+ db.run('DELETE FROM houses WHERE owner_id = ? AND completed = ?', [
+   req.user.id,
+   1
+ ], function(err) {
+   if (err) { return next(err); }
+   return res.redirect('/' + (req.body.filter || ''));
+ });
+});
 // Get Sell Page
 module.exports = router;
